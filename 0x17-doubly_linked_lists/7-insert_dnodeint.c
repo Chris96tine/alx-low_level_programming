3f15ff17311974d44451d1a0bde54347367c6729
Chris96tine <lumonyechristine@gmail.com>
2022-06-09 18:09:52 +0300

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,54 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index- deletes the node at a given index
- * @head: pointer to the list
- * @index: index of the node to delete
- * Return: 1 if it succeeded, -1 if it failed
-*/
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *tmp;
-dlistint_t *tmp2;
-unsigned int i;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-if (head == NULL)
-return (-1);
-if (*head == NULL)
-return (-1);
-if (index == 0)
-{
-tmp = *head;
-*head = (*head)->next;
-if (*head != NULL)
-(*head)->prev = NULL;
-free(tmp);
-return (1);
-}
-tmp = *head;
-i = 0;
-while (tmp != NULL)
-{
-if (i == index - 1)
-{
-tmp2 = tmp->next;
-tmp->next = tmp->next->next;
-if (tmp->next != NULL)
-tmp->next->prev = tmp;
-free(tmp2);
-return (1);
-}
-tmp = tmp->next;
-i++;
-}
-return (-1);
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
 
-return (1);
+	return (new);
 }
